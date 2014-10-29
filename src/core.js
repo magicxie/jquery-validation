f303479b93b5b706dea51e1c646ef8b4780822c6
magic.xie <eggcanfly@gmail.com>
2014-10-29 14:31:54 +0800

    Update core.js
    browser compatible for date rule

--- src/core.js
+++ src/core.js
@@ -1065,7 +1065,7 @@ $.extend($.validator, {
 
 		// http://jqueryvalidation.org/date-method/
 		date: function( value, element ) {
-			return this.optional(element) || !/Invalid|NaN/.test(new Date(value).toString());
+			return this.optional(element) || !/Invalid|NaN/.test(new Date(value.replace(/-/g),'/').toString());
 		},
 
 		// http://jqueryvalidation.org/dateISO-method/
